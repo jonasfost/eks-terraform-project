6546fa66f51f34e38b74be1e4d9ecf817bc235f9
Jonathan Fotso <jonasfost@gmail.com>
2024-01-12 14:40:03 -0500

    Terraform files updated

--- terraform/data.tf
+++ terraform/data.tf
@@ -11,9 +11,9 @@ data "aws_iam_user" "user_name" {
 }
 
 data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_id
+  name = module.eks.cluster_name
 }
 
 data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_id
+  name = module.eks.cluster_name
 }
